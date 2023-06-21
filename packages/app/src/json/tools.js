f79d30cde1b98ea229e77fbaa8223061a3c8d82e
leoncan122 <leon.can122@gmail.com>
2023-06-21 12:21:10 +0200

    add json

---
+++ packages/app/src/json/tools.js
@@ -0,0 +1,45 @@
+export const Tools = [
+    {
+      name: '',
+      decription: '',
+      image: '',
+      details: {
+          categories: [],
+          sourceURL: '',
+          repositoryURL: '',
+          supportedBy: ''
+      },
+      usedReleases: [
+          {
+              version: '0.0.0',
+              releaseDate: '' 
+          }
+      ],
+      content: {
+          definition: '',
+          sourceURL: ''
+      }
+    },
+  ];
+  
+  // {
+  //     name: '',
+  //     decription: '',
+  //     image: '',
+  //     details: {
+  //         categories: [],
+  //         sourceURL: '',
+  //         repositoryURL: '',
+  //         supportedBy: ''
+  //     },
+  //     usedReleases: [
+  //         {
+  //             version: '0.0.0',
+  //             releaseDate: '' 
+  //         }
+  //     ],
+  //     content: {
+  //         definition: '',
+  //         sourceURL: ''
+  //     }
+  //   },
