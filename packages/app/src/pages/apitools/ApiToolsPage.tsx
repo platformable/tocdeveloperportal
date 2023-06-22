bb3c34b2dbd08fe1f9dffb219b438c7b0b5a22be
Alexei Garban <garban.valdeon@gmail.com>
2023-06-22 12:13:22 +0200

    update markdown

--- packages/app/src/pages/apitools/ApiToolsPage.tsx
+++ packages/app/src/pages/apitools/ApiToolsPage.tsx
@@ -53,15 +53,73 @@ export interface IApiTool  {
   releases: Array<{version: string, releaseDate: string}>
   content: string
 }
+
+
+
+
+
+
 export default function page() {
   let { companyName } = useParams();
+
+  const Tools = [
+    {
+      name: 'Stoplight',
+      description: 'Design first, Develop Better',
+      image: 'https://res.cloudinary.com/platformable/image/upload/v1687341510/internaldevportal/spotlight/rq7iui3t9b3hcf4a3bru.jpg',
+      details: {
+          categories: ['API','Design','API Documentation'],
+          links: [
+            {
+                sourceName: 'Github',
+                url: 'https://github.com/Midships/digital-txn-signing'
+            }
+            //...more links
+          ],
+          supportedBy: ['Security']
+      },
+      releases: [
+          {
+              version: '0.0.0',
+              releaseDate: 'May 2023' 
+          }
+      ],
+      content: `
+# Include the next code under metadata -> annotations:
+\n![Drag Racing](https://raw.githubusercontent.com/Midships/digital-txn-signing/master/images/passwordless_enroll.png)
+\n
+Create a “/docs” folder in template root directory and create a index.md. You can use this as starter:
+\n
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur arcu mi, euismod non sagittis ac, fringilla non diam. Phasellus suscipit fringilla venenatis. Morbi mi libero, dictum ut purus id, luctus consectetur turpis. Nullam eu nisi et ante commodo hendrerit. Nulla consequat ipsum eu felis suscipit, ultrices viverra nibh maximus. Curabitur mauris mauris, scelerisque sit amet suscipit a, suscipit quis libero. Pellentesque elementum pulvinar metus laoreet eleifend.
+\n
+Nullam feugiat nisi eget placerat consectetur. In hac habitasse platea dictumst. Fusce at dui laoreet, molestie ex a, consequat sapien. Proin eget lectus rutrum, viverra enim a, venenatis mauris. Proin nec tortor in lacus sollicitudin viverra. Duis eget dui pulvinar, vehicula dui id, scelerisque lorem. Proin non mauris vel eros bibendum tempor eu quis ipsum. Praesent faucibus ex sed purus bibendum, a varius enim hendrerit. Fusce sed est et lorem ultricies tincidunt. Pellentesque dolor eros, imperdiet at nunc at, posuere elementum enim. Sed mauris augue, maximus at imperdiet vel, pharetra nec dolor. Aliquam vitae sem cursus, ornare libero a, tempus metus. Quisque in est in mi gravida condimentum. Suspendisse vehicula tempus pellentesque.
+\n
+# Our contact details
+Steps:
+
+1. Create [yaml file](https://github.com/leoncan122/followMe/blob/master/apis/openapi-example.yaml) to create the entitie and store in local folder or github
+2. Define url or file path at Location's section in app-config.yaml
+
+### For any additional support please contact Midships using the following channels:
+
+Website: www.midships.io\n
+Email: support@midships.io / sales@midships.io
+\n
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](https://www.youtube.com/watch?v=7rgvapDYPo0)
+\n
+
+
+      `
+    },
+  ];
 
   const data: IApiTool = useMemo(() => Tools.find(tool => tool.name === companyName), [companyName])!
 
   console.log('userid', data);
   const classes = useStyles();
 
-const markdown = `# Design, Document & Build APIs Faster**.
+const markdown = `# **Design, Document & Build APIs Faster**.
 A set of authentication trees that enable native Digital Transaction Signing on ForgeRock's Identity Platform 7.x and ForgeRock Identity Cloud. Please contact us at sales@midships.io for further details of our solution.
 
 The Midships’ Digital transaction signing solution can be used by those customers looking to introduce non-repudiation across transactions e.g. payments, changes to sensitive details and/or to comply with regulatory standards such as MAS 14.2.3 and MAS 14.2.4 TRM-Guidelines-18-January-2021.pdf (mas.gov.sg):
@@ -122,6 +180,13 @@ The example flow below validates the users username and password and enrolls the
 
 ![Drag Racing](https://raw.githubusercontent.com/Midships/digital-txn-signing/master/images/passwordless_enroll.png)
   `
+
+
+  
+console.log("markdown", typeof markdown)
+
+console.log("tools",  Tools[0].content)
+  
   return (
     <Container maxWidth={'xl'} classes={{}}>
       <div
@@ -143,14 +208,14 @@ The example flow below validates the users username and password and enrolls the
         <Grid container spacing={4}>
           <Grid item xs={8}>
             <Card className={classes.root}>
-              <CardActionArea>
+             
                 <CardContent>
                   <div >
-                  <ReactMarkdown  children={data.content} />
+                  <ReactMarkdown  children={Tools[0].content} />
                     
                   </div>
                 </CardContent>
-              </CardActionArea>
+            
             </Card>
           </Grid>
           <Grid item xs={4}>
