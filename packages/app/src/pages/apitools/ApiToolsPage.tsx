2b077daf62dcc8c601f8a450c45438133d79dc66
leoncan122 <leon.can122@gmail.com>
2023-06-23 16:17:21 +0200

    ckean toolspage

--- packages/app/src/pages/apitools/ApiToolsPage.tsx
+++ packages/app/src/pages/apitools/ApiToolsPage.tsx
@@ -68,56 +68,7 @@ export default function page() {
       link.setAttribute('target', '_blank')
     });
   }, []);
-//   const Tools = [
-//     {
-//       name: 'Stoplight',
-//       description: 'Design first, Develop Better',
-//       image: 'https://res.cloudinary.com/platformable/image/upload/v1687341510/internaldevportal/spotlight/rq7iui3t9b3hcf4a3bru.jpg',
-//       details: {
-//           categories: ['API','Design','API Documentation'],
-//           links: [
-//             {
-//                 sourceName: 'Github',
-//                 url: 'https://github.com/Midships/digital-txn-signing'
-//             }
-//             //...more links
-//           ],
-//           supportedBy: ['Security']
-//       },
-//       releases: [
-//           {
-//               version: '0.0.0',
-//               releaseDate: 'May 2023' 
-//           }
-//       ],
-//       content: `
-// # Include the next code under metadata -> annotations:
-// \n[![Drag Racing](https://raw.githubusercontent.com/Midships/digital-txn-signing/master/images/passwordless_enroll.png)](https://raw.githubusercontent.com/Midships/digital-txn-signing/master/images/passwordless_enroll.png)
-// \n
-// Create a “/docs” folder in template root directory and create a index.md. You can use this as starter:
-// \n
-// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur arcu mi, euismod non sagittis ac, fringilla non diam. Phasellus suscipit fringilla venenatis. Morbi mi libero, dictum ut purus id, luctus consectetur turpis. Nullam eu nisi et ante commodo hendrerit. Nulla consequat ipsum eu felis suscipit, ultrices viverra nibh maximus. Curabitur mauris mauris, scelerisque sit amet suscipit a, suscipit quis libero. Pellentesque elementum pulvinar metus laoreet eleifend.
-// \n
-// Nullam feugiat nisi eget placerat consectetur. In hac habitasse platea dictumst. Fusce at dui laoreet, molestie ex a, consequat sapien. Proin eget lectus rutrum, viverra enim a, venenatis mauris. Proin nec tortor in lacus sollicitudin viverra. Duis eget dui pulvinar, vehicula dui id, scelerisque lorem. Proin non mauris vel eros bibendum tempor eu quis ipsum. Praesent faucibus ex sed purus bibendum, a varius enim hendrerit. Fusce sed est et lorem ultricies tincidunt. Pellentesque dolor eros, imperdiet at nunc at, posuere elementum enim. Sed mauris augue, maximus at imperdiet vel, pharetra nec dolor. Aliquam vitae sem cursus, ornare libero a, tempus metus. Quisque in est in mi gravida condimentum. Suspendisse vehicula tempus pellentesque.
-// \n
-// # Our contact details
-// Steps:
 
-// 1. Create [yaml file](https://github.com/leoncan122/followMe/blob/master/apis/openapi-example.yaml) to create the entitie and store in local folder or github
-// 2. Define url or file path at Location's section in app-config.yaml
-
-// ### For any additional support please contact Midships using the following channels:
-
-// Website: www.midships.io\n
-// Email: support@midships.io / sales@midships.io
-// \n
-// [![Watch the video](https://img.youtube.com/vi/7rgvapDYPo0/hqdefault.jpg)](https://www.youtube.com/watch?v=7rgvapDYPo0)
-// \n
-
-
-//       `
-//     },
-//   ];
 
   const data: IApiTool = useMemo(() => Tools.find(tool => tool.name === companyName), [companyName])!
 
@@ -127,7 +78,7 @@ export default function page() {
   
   return (
     <Container maxWidth={'xl'} classes={{}}>
-      {/* <base target="_blank" /> */}
+ 
       <div
         className={classes.root}
         style={{ marginTop: 30, marginBottom: 100, paddingBottom: 100 }}
